c4b062ee38655c6f046d06ccaf7a05b5a833632d
Amauri Barros <36938889+melobarros@users.noreply.github.com>
2020-04-01 17:08:17 -0300

    Implemented 1-pass solution

--- Array/TwoSum.py
+++ Array/TwoSum.py
@@ -10,3 +10,17 @@ Example:
   Because nums[0] + nums[1] = 2 + 7 = 9,
   return [0, 1].
 """
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        
+        for index, num in enumerate(nums):
+            other = target - num
+            
+            if other in seen:
+                return [seen[other], index]
+            else:
+                seen[num] = index
+                
+        return []
