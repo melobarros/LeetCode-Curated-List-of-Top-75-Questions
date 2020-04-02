2e6d07deca71a86f8ae2c734f3c0b1beecf2b94d
Amauri Barros <36938889+melobarros@users.noreply.github.com>
2020-04-02 15:18:07 -0300

    Update Two-Sum.py

--- Array/Two-Sum.py
+++ Array/Two-Sum.py
@@ -1,5 +1,6 @@
 """
-Two Sum - LeetCode Easy
+Two Sum
+LeetCode Easy Problem
 
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
