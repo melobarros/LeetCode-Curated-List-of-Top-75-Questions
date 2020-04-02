0267e39badce8c15612a974e9b62dedf50f3ac9e
Amauri Barros <36938889+melobarros@users.noreply.github.com>
2020-04-02 15:17:26 -0300

    Update Best-Time-to-Buy-and-Sell-Stock.py

--- Array/Best-Time-to-Buy-and-Sell-Stock.py
+++ Array/Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,5 +1,6 @@
 """
-Best Time to Buy and Sell Stock - LeetCode Easy
+Best Time to Buy and Sell Stock 
+LeetCode Easy Problem
 
 Say you have an array for which the ith element is the price of a given stock on day i.
 If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), 
